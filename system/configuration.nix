c69391a44375fa5830b015a32f981ec46609bfd0
siph <dawkins.chris.dev@gmail.com>
2022-07-25 11:41:49 -0600

    refactor: formatting

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,32 +1,29 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # Bootloader.
   boot.loader.grub = {
-      enable = true;
-      device = "/dev/sda";
-      useOSProber  = false;
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = false;
   };
 
   networking = {
-      hostName = "nixos";
-      networkmanager.enable = true;
-  #    wireless.enable = true; # Enables wireless support via wpa_supplicant.
+    hostName = "nixos";
+    networkmanager.enable = true;
+    #    wireless.enable = true;
   };
 
-  # Set your time zone.
   time.timeZone = "America/Denver";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   services = {
@@ -35,7 +32,7 @@
       xkbVariant = "";
       enable = true;
       displayManager.lightdm.enable = true;
-      desktopManager.plasma5.enable = true;
+      #desktopManager.plasma5.enable = true;
     };
     pipewire = {
       enable = true;
@@ -43,25 +40,30 @@
       alsa.support32Bit = true;
       pulse.enable = true;
     };
+    openssh = {
+      enable = true;
+    };
   };
 
+  programs = {
+    zsh = {
+      enable = true;
+    };
+  };
 
-  # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.chris = {
-    isNormalUser = true;
-    description = "chris";
-    extraGroups = [ "networkmanager" "wheel" ];
+  users = {
+    defaultUserShell = pkgs.zsh;
+    users.chris = {
+      isNormalUser = true;
+      description = "chris";
+      extraGroups = ["networkmanager" "wheel"];
+    };
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   nix = {
@@ -71,30 +73,11 @@
     '';
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  git
-    gparted
+    vim
+    git
     wget
-    firefox
-    neovim
   ];
-
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -103,5 +86,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.05"; # Did you read the comment?
-
 }
