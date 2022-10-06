da3c7f2245d437d341d64fd5952935b23c0c6fc2
siph <dawkins.chris.dev@gmail.com>
2022-10-06 11:31:37 -0600

    fix deprecated package location
    The flake package was seemingly moved to stable.

--- system/media/configuration.nix
+++ system/media/configuration.nix
@@ -138,7 +138,7 @@
   };
 
   nix = {
-    package = pkgs.nixFlakes; # or versioned attributes like nixVersions.nix_2_8
+    package = pkgs.nixVersions.stable; # or versioned attributes like nixVersions.nix_2_8
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
--- system/nixos/configuration.nix
+++ system/nixos/configuration.nix
@@ -143,7 +143,7 @@
   };
 
   nix = {
-    package = pkgs.nixFlakes; # or versioned attributes like nixVersions.nix_2_8
+    package = pkgs.nixVersions.stable; # or versioned attributes like nixVersions.nix_2_8
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
