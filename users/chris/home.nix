881449469e8342003559dfc70f1de090e3a4aab6
siph <dawkins.chris.dev@gmail.com>
2022-10-11 12:36:53 -0600

    zsh module

--- users/chris/home.nix
+++ users/chris/home.nix
@@ -7,6 +7,7 @@
   # paths it should manage.
   imports = [
     ../../modules/programs/git
+    ../../modules/programs/zsh
   ];
   home = {
     username = "chris";
@@ -106,50 +107,6 @@
     # Let Home Manager install and manage itself.
     home-manager.enable = true;
     # Shell
-    zsh = {
-      enable = true;
-      enableAutosuggestions = true;
-      enableCompletion = true;
-      enableSyntaxHighlighting = true;
-      plugins = [
-      # This will make `nix-shell` use zsh instead of bash.
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.5.0";
-          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
-         };
-       }
-      ];
-      envExtra = "
-        export EDITOR='nvim'
-        export BROWSER='firefox'
-        export MANPAGER='nvim +Man!'
-        export TERM='xterm-256color'
-
-        # temporary paths
-        export PATH=~/.local/bin:$PATH
-        export PATH=~/Downloads/dwarfs-0.6.1-Linux/bin:$PATH
-        export PATH=~/Downloads/dwarfs-0.6.1-Linux/sbin:$PATH
-        export PATH=~/Downloads/dwarfs-0.6.1-Linux/share:$PATH
-        export PATH=~/repos/neovim-flake/result/bin:$PATH
-      ";
-      initExtra = "
-        alias vim='nvim'
-        alias cat='bat'
-        alias startx-dwm='startx ~/.xinitrc dwm'
-        alias startx-kde='startx ~/.xinitrc kde'
-
-        # Misc
-        zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}'
-        bindkey -v
-        # Colors
-        autoload -Uz colors && colors
-      ";
-    };
     bash = {
       enable = true;
     };
