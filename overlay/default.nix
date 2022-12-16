c678d1da902e66b984dbc2651a3e59bf3962f0c6
siph <dawkins.chris.dev@gmail.com>
2022-12-15 17:27:18 -0700

    glow overlay
    Glow doesn't render linebreaks. Overlay until
    https://github.com/charmbracelet/glow/pull/362 is merged.

--- overlay/default.nix
+++ overlay/default.nix
@@ -16,6 +16,22 @@ let
         sha256 = "0oXZj+mHUPcCCniWk6uwIcvfpRQcKT05CAlk1KsnMAo=";
       };
     });
+    glow =
+        let
+          version = "3c36258be77d6c636040e7d37b600a2daa2d168b";
+          src = pkgs.fetchFromGitHub {
+            owner = "WieeRd";
+            repo = "glow";
+            rev = "3c36258be77d6c636040e7d37b600a2daa2d168b";
+            sha256 = "x6NEboTGqp3OwCjm2fcm6MUEhcIOuai1Qvl3s0Totr4=";
+          };
+        in
+          (pkgs.callPackage "${pkgs.path}/pkgs/applications/editors/glow" {
+            buildGoModule = args: pkgs.buildGoModule (args // {
+              vendorSha256 = "JCqC494VN60ij65G2+4PRoQJib+fRV+kiOWgSO45e7M=";
+              inherit src version;
+            });
+          });
     neovim = inputs.chris-neovim.packages.x86_64-linux.default;
     #inherit (inputs.veloren.packages."x86_64-linux") veloren-voxygen;
   };
