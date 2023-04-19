fbd72419374e44cd2f06d823b29442a3b4ae1411
siph <dawkins.chris.dev@gmail.com>
2023-04-18 18:11:43 -0600

    firefox css

--- home-manager/chris/applications/misc/firefox/default.nix
+++ home-manager/chris/applications/misc/firefox/default.nix
@@ -1,6 +1,21 @@
-{ lib, pkgs, ... }:
+{ ... }:
 {
   programs.firefox = {
     enable = true;
+    profiles.default = {
+      isDefault = true;
+      userChrome = ''
+        @import "${
+          builtins.fetchGit {
+            url = "https://github.com/rockofox/firefox-minima";
+            ref = "main";
+            rev = "1477b2a28091aad4ebba330c539110c311eb8084";
+          }
+        }/userChrome.css";
+      '';
+      settings = {
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      };
+    };
   };
 }
