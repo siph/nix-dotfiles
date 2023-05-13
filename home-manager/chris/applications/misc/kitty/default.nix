fb5f5cd2f7b3b2ea97f6f443e4dcd07df7b75705
siph <dawkins.chris.dev@gmail.com>
2023-05-13 16:09:53 -0600

    fix fonts

--- home-manager/chris/applications/misc/kitty/default.nix
+++ home-manager/chris/applications/misc/kitty/default.nix
@@ -1,18 +1,17 @@
-{ lib, pkgs, ... }:
+{ ... }:
 {
   programs.kitty = {
     enable = true;
     theme = "Gruvbox Dark Hard";
-    font = {
-      name = "JetBrains Mono NL ExtraBold Nerd Font Complete";
-      size = 9;
-    };
     settings = {
+      font_family = "JetBrainsMono NF ExtraBold";
+      font_size = 9;
       enable_audio_bell = false;
       visual_bell_duration = 0;
       window_alert_on_bell = false;
       background_opacity = "0.85";
       confirm_os_window_close = 0;
+      text_composition_strategy = "legacy";
     };
   };
 }
