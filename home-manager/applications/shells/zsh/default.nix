bab93641f78f0021692ae2656f7954cba8c4f967
siph <dawkins.chris.dev@gmail.com>
2023-08-21 16:24:04 -0600

    cleanup zsh

--- home-manager/applications/shells/zsh/default.nix
+++ home-manager/applications/shells/zsh/default.nix
@@ -1,40 +1,20 @@
-{ pkgs, ... }:
 {
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
-    plugins = [
-      # This will make `nix-shell` use zsh instead of bash.
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.5.0";
-          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
-        };
-      }
-    ];
-    envExtra = "
-      export EDITOR='nvim'
-      export BROWSER='firefox'
-      export MANPAGER='nvim +Man!'
-      export TERM='xterm-256color'
-    ";
-    initExtra = "
-      alias cat='bat'
-      alias startx-kde='startx ~/.xinitrc kde'
-      alias startx-xmonad='startx ~/.xinitrc xmonad'
-      alias tree='broot'
-
-      # Misc
-      zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}'
-      bindkey -v
-      # Colors
-      autoload -Uz colors && colors
-    ";
+    defaultKeymap = "viins";
+    sessionVariables = {
+      EDITOR = "nvim";
+      BROWSER = "firefox";
+      MANPAGER = "nvim +Man!";
+      TERM = "xterm-256color";
+    };
+    shellAliases = {
+      cat = "bat";
+      tree = "broot";
+    };
+    initExtra = "zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}'";
   };
 }
