329d27543d6aed1c16902d723c3f50acaf1c19e0
siph <dawkins.chris.dev@gmail.com>
2023-08-23 18:10:08 -0600

    nushell overlay
    Until `0.84.0` is in `nixpkgs`.

--- overlay/default.nix
+++ overlay/default.nix
@@ -13,6 +13,20 @@ let
     });
     neovim = inputs.chris-neovim.packages.${system}.default;
     yt-watcher = inputs.yt-watcher.packages.${system}.default;
+    nushell = prev.nushell.overrideAttrs (oldAttrs: rec {
+      src = inputs.nixpkgs.legacyPackages.${system}.fetchFromGitHub {
+        owner = "nushell";
+        repo = "nushell";
+        version = "0.84.0";
+        rev = "d2abb8603abab43ec4a6d4ed56f9018790b99dce";
+        sha256 = "sha256-vXtQUWKRPS53IBUgO9Dw8dVzfD5W2kHSPOZHs293O5Q=";
+      };
+      cargoDeps = oldAttrs.cargoDeps.overrideAttrs (prev.lib.const {
+        inherit src;
+        name = "nushell-d2abb8603abab43ec4a6d4ed56f9018790b99dce-vendor.tar.gz";
+        outputHash = "sha256-BTK7qJjwvtpQek3BF7IIekkH1e1lamFh5BvpPj7wA84=";
+      });
+    });
   };
 in
 inputs.nixpkgs.lib.composeManyExtensions [ additions modifications ]
