2ab3b8b64df47409162835d8ab35afd4db67048d
siph <dawkins.chris.dev@gmail.com>
2023-08-27 11:02:27 -0600

    nix-community cachix

--- nixos/desktop/cachix/default.nix
+++ nixos/desktop/cachix/default.nix
@@ -4,5 +4,6 @@
   ];
   cachix = [
     # { name = "veloren-nix"; sha256 = "1yv09n45bgmvkjxm6jq8v6js5qy5y85d4cn2ayn60bp48mas2bar"; }
+    { name = "nix-community"; sha256 = "0m6kb0a0m3pr6bbzqz54x37h5ri121sraj1idfmsrr6prknc7q3x"; }
   ];
 }
