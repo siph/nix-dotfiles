43e7ebce16dd310ea49ea6f683ae2775d133e82b
siph <dawkins.chris.dev@gmail.com>
2023-09-04 13:48:34 -0600

    qtile start-script

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,4 +7,5 @@
   startx-xmonad = pkgs.callPackage ./start-scripts/xmonad { };
   startx-kde = pkgs.callPackage ./start-scripts/kde { };
   start-hyprland = pkgs.callPackage ./start-scripts/hyprland { };
+  startx-qtile = pkgs.callPackage ./start-scripts/qtile { };
 }
---
+++ pkgs/start-scripts/qtile/default.nix
@@ -0,0 +1,13 @@
+{ pkgs }:
+pkgs.stdenv.mkDerivation rec {
+  name = "startx-qtile";
+  src = ../../../home-manager/applications/window-managers/qtile/xinit;
+  buildInputs = with pkgs; [ makeWrapper xorg.xinit ];
+  installPhase = ''
+    mkdir -p $out/bin
+    mkdir -p $out/xinit
+    cp ./xinitrc.sh $out/xinit/xinit_${name}
+    makeWrapper ${pkgs.xorg.xinit}/bin/startx $out/bin/${name} \
+     --add-flags $out/xinit/xinit_${name}
+  '';
+}
