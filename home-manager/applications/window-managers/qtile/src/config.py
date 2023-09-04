949e259f1f9d92f95da030656a985f925507f978
siph <dawkins.chris.dev@gmail.com>
2023-09-04 15:31:37 -0600

    remove floating rules

--- home-manager/applications/window-managers/qtile/src/config.py
+++ home-manager/applications/window-managers/qtile/src/config.py
@@ -36,7 +36,6 @@ keys = [
         desc="Toggle fullscreen on the focused window",
     ),
 
-
     Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
 ]
@@ -188,13 +187,7 @@ cursor_warp = False
 floating_layout = layout.Floating(
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
-        *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),  # gitk
-        Match(wm_class="makebranch"),  # gitk
-        Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
-        Match(title="branchdialog"),  # gitk
-        Match(title="pinentry"),  # GPG key password entry
+        # Match(title="pinentry"),  # GPG key password entry
     ]
 )
 auto_fullscreen = True
