644e9dddcc3ec834fa79c70cd2f7f4ef452c20aa
siph <dawkins.chris.dev@gmail.com>
2023-11-15 17:02:10 -0700

    remove stig
    It's breaking my config, idk why.

--- home-manager/applications/editors/neovim/default.nix
+++ home-manager/applications/editors/neovim/default.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     neovim # Use custom neovim build from overlay.
+    ripgrep
+    fd
     wl-clipboard
     xsel
   ];
--- home-manager/applications/tools/misc/default.nix
+++ home-manager/applications/tools/misc/default.nix
@@ -4,13 +4,10 @@
   home.packages = with pkgs; [
     bluetuith
     duf
-    fd
     gh
     glow
     ouch
-    ripgrep
     repgrep
-    stig
     tldr
   ];
 }
