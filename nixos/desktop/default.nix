19b4601944468c0562e2fadd65c35a15c49d4c88
siph <dawkins.chris.dev@gmail.com>
2023-12-01 15:13:15 -0700

    dconf

--- nixos/desktop/default.nix
+++ nixos/desktop/default.nix
@@ -99,9 +99,12 @@
     };
     pipewire = {
       enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
       pulse.enable = true;
+      jack.enable = true;
       wireplumber.enable = true;
     };
     openssh = {
@@ -151,6 +154,7 @@
       enableSSHSupport = true;
       pinentryFlavor = "curses";
     };
+    dconf.enable = true;
     hyprland = {
       enable = false;
     };
