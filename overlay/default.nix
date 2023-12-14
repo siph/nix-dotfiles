bc67e10efe1f717530dbd0c12d67bb3bb946278e
siph <dawkins.chris.dev@gmail.com>
2023-12-14 16:51:08 -0700

    nu_scripts overlay

--- overlay/default.nix
+++ overlay/default.nix
@@ -6,9 +6,24 @@ let
     waybar = prev.waybar.overrideAttrs (oldAttrs: {
       mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
     });
+
     neovim = chris-neovim.packages.${prev.system}.default;
     yt-watcher = yt-watcher.packages.${prev.system}.default;
     wt-fetch = wt-fetch.packages.${prev.system}.default;
+
+    # https://github.com/nushell/nu_scripts/pull/669
+    # This PR broke my config. I'll figure it out and fix it later™.
+    nu_scripts = prev.nu_scripts.overrideAttrs (finalAttrs: previousAttrs: rec {
+      version = "99fe279311cf3028930522c0646f9bc70e4451de";
+
+      src = prev.fetchFromGitHub {
+        owner = "nushell";
+        repo = previousAttrs.pname;
+        rev = "${version}";
+        hash = "sha256-w1lkdccC3ZzLaUqrQh+QW/H3V23dlhtd+16kkCoOtKI=";
+      };
+    });
+
     # nest a stable `nixpkgs` release inside of unstable
     nixpkgs-stable = nixpkgs-stable.legacyPackages.${prev.system};
   };
