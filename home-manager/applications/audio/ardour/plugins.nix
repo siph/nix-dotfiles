8bba2b4cbfb4eac3638e671fa105fb8325040687
siph <dawkins.chris.dev@gmail.com>
2024-01-01 22:45:37 -0700

    ardour plugins

--- home-manager/applications/audio/ardour/plugins.nix
+++ home-manager/applications/audio/ardour/plugins.nix
@@ -1,19 +1,30 @@
 ### Plugins for ardour
 {pkgs, ...}: {
   plugins = with pkgs; [
-    lsp-plugins
-    caps
+    artyFX
     bchoppr
+    bjumblr
+    boops
     bsequencer
     bshapr
     bslizr
+    caps
+    distrho
     drumgizmo
+    drumkv1
+    fmsynth
+    fomp
+    gxplugins-lv2
     helm
     infamousPlugins
+    kapitonov-plugins-pack
+    lsp-plugins
+    mda_lv2
     ninjas2
+    samplv1
     swh_lv2
+    tunefish
     x42-avldrums
     x42-plugins
-    lsp-plugins
   ];
 }
