063cd7de57643a5d129a3d24ca9c647d782ef100
siph <dawkins.chris.dev@gmail.com>
2024-03-30 16:55:23 -0600

    yt-watcher service

--- home-manager/applications/tools/yt-dlp/default.nix
+++ home-manager/applications/tools/yt-dlp/default.nix
@@ -1,9 +1,20 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [ffmpeg yt-watcher];
+
   programs.yt-dlp = {
     enable = true;
     settings = {
       continue = true;
     };
+  };
+
+  systemd.user.services.yt-watcher = with pkgs; {
+    Unit.Description = "Youtube auto-downloader";
+
+    Install.WantedBy = ["default.target"];
+
+    Service.ExecStart = ''
+      ${pkgs.yt-watcher}/bin/yt-watcher /home/chris/.config/yt-watcher/config.yaml
+    '';
   };
 }
