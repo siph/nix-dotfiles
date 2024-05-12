0f8f9cc086fd028cae7405e9e995cab5e9f75c90
siph <dawkins.chris.dev@gmail.com>
2024-05-12 12:09:02 -0600

    mpv loop

--- home-manager/applications/video/mpv/default.nix
+++ home-manager/applications/video/mpv/default.nix
@@ -9,6 +9,7 @@
         ao = "pulse";
         cache = true;
         force-seekable = true;
+        loop = true;
         script-opts = [
           "ytdl_hook-ytdl_path=${pkgs.yt-dlp}/bin/yt-dlp"
           # Thumbnails (Press `T` to generate)
