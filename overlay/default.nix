58d57e137ff18b0f27e60ed082a1f4a5d141ce79
siph <dawkins.chris.dev@gmail.com>
2024-06-06 09:11:00 -0600

    remove invidious overlay

--- overlay/default.nix
+++ overlay/default.nix
@@ -17,17 +17,6 @@
     yt-watcher = yt-watcher.packages.${prev.system}.default;
     wt-fetch = wt-fetch.packages.${prev.system}.default;
 
-    invidious = prev.invidious.overrideAttrs (_oldAttrs: {
-      src = prev.fetchFromGitHub {
-        # ChunkyProgrammer:add-support-for-comment-format
-        owner = "ChunkyProgrammer";
-        repo = "invidious";
-        fetchSubmodules = true;
-        rev = "eda7444ca46dbc3941205316baba8030fe0b2989";
-        sha256 = "sha256-YZ+uhn1ESuRTZxAMoxKCpxEaUfeCUqOrSr3LkdbrTkU=";
-      };
-    });
-
     # https://github.com/nushell/nu_scripts/pull/669
     # This PR broke my config. I'll figure it out and fix it later™.
     nu_scripts = prev.nu_scripts.overrideAttrs (finalAttrs: previousAttrs: rec {
