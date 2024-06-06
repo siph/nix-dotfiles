dbae78699ca1213d280dd532f681ab057ac90199
siph <dawkins.chris.dev@gmail.com>
2024-06-06 10:07:14 -0600

    unciv

--- home-manager/applications/games/default.nix
+++ home-manager/applications/games/default.nix
@@ -3,13 +3,13 @@
     ./lutris
     ./starsector
     ./steam
+    ./unciv
     ./veloren
     # ./openttd
     # ./cataclysm-dda-git
     # ./cave-story
     # ./freeciv
-    #./wesnoth
-    #./dwarf-fortress
+    # ./wesnoth
     # These are all broken
     #./freeorion
     #./keeperrl
