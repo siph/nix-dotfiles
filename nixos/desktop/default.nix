bab5a1546920320d4ca7f81cf62a84e9cc1823a5
siph <dawkins.chris.dev@gmail.com>
2025-01-01 15:00:22 -0700

    ghostty cachix

--- nixos/desktop/default.nix
+++ nixos/desktop/default.nix
@@ -40,10 +40,12 @@
       substituters = [
         "https://nix-community.cachix.org"
         "https://surrealdb.cachix.org"
+        "https://ghostty.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "surrealdb.cachix.org-1:rbm7Qs+s36pxbfk9jhIa5HRld6gZ63koZz1h/9sSxaA="
+        "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
       ];
     };
   };
