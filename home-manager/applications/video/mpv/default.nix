a8de4881064e86302a7c2c7123a529a9ea281562
siph <dawkins.chris.dev@gmail.com>
2025-06-03 17:23:28 -0600

    fix mpv

--- home-manager/applications/video/mpv/default.nix
+++ home-manager/applications/video/mpv/default.nix
@@ -9,6 +9,7 @@
         cache = true;
         force-seekable = true;
         loop = true;
+        gpu-api = "opengl";
         script-opts = [
           "ytdl_hook-ytdl_path=${pkgs.yt-dlp}/bin/yt-dlp"
           # Thumbnails (Press `T` to generate)
